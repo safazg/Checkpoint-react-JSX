ba9001fd3d19896c27b2cb5263db6e3ec56a56cf
ciao-boop <safa.zgaya@gmail.com>
2021-01-28 03:35:20 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import imageInSrc from "./imageInSrc.jpg"
+import "./style.css"
+function App() {
+  return (
+    <div className="App">
+      <div style={{border:"solid 1px black", maxWidth:"100vm"}}>
+        <h1 className="title red">Your name here</h1>
+        <br/> 
+        <img src={imageInSrc} />
+        <br/>
+        <img src="/imageInPublic.jpg" />
+      </div>
+      <video width={320} height={240} controls>
+          <source src="/myVideo.mp4" type="video/mp4" />
+      </video>
+          
+    
+    </div>
+  );
+}
+
+export default App;
